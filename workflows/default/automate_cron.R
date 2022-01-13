b1fafb906f25de1c85f7325a611475b770ede6f0
rqthomas <rqthomas@vt.edu>
2022-01-13 18:22:33 +0000

    updates for automation

--- workflows/default/automate_cron.R
+++ workflows/default/automate_cron.R
@@ -3,13 +3,14 @@
 library(cronR)
 
 lake_directory <- here::here()
+config_set_name <- "default"
 
-run_config <- yaml::read_yaml(file.path(lake_directory,"configuration","FLAREr","configure_run.yml"))
-forecast_site <- run_config$forecast_site
+home_dir <- file.path(lake_directory, "workflows", config_set_name)
 
-cmd <- cronR::cron_rscript(rscript = file.path(lake_directory, "automation", "combined_workflow.R"),
-                           rscript_log = file.path(lake_directory, "automation", paste0(forecast_site, ".log")),
+cmd <- cronR::cron_rscript(rscript = file.path(home_dir, "combined_workflow.R"),
+                           rscript_log = file.path(home_dir, "fcre.log"),
                            log_append = FALSE,
-                           workdir = file.path(lake_directory, "automation"))
+                           workdir = file.path(home_dir))
 #trailing_arg = "curl -fsS -m 10 --retry 5 -o /dev/null https://hc-ping.com/cb249e47-f56b-45da-af7f-9c0c47db1a6c")
-cronR::cron_add(command = cmd,  frequency = 'hourly', id = paste0(forecast_site, "_forecast"))
+cronR::cron_add(command = cmd,  frequency = 'hourly', id = 'fcre_forecast')
+
